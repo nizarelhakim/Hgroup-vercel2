b34abefbc0bf8d76fa0692d4b3efe80e6fcae2f9
nizarelhakim <nizarhakim@gmail.com>
2025-04-23 12:11:48 +0400

    Update HGroupHomePage.jsx

--- src/HGroupHomePage.jsx
+++ src/HGroupHomePage.jsx
@@ -1,5 +1,5 @@
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "./components/ui/card";
+import { Button } from "./components/ui/button";
 import { motion } from "framer-motion";
 
 export default function HGroupHomePage() {
