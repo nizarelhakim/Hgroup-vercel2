3b3c4361e07444f35d74928f29ad960ced53258f
nizarelhakim <nizarhakim@gmail.com>
2025-04-23 12:59:31 +0400

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,6 @@
-const postcss = require('postcss');
-const tailwindcss = require('@tailwindcss/postcss');
-const autoprefixer = require('autoprefixer');
-
 module.exports = {
-  plugins: [tailwindcss, autoprefixer],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
